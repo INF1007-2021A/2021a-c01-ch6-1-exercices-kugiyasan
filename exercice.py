53844211d826798666dab96efb289d4d54b49bf8
kugiyasan <kugiyasan@users.noreply.github.com>
2021-09-23 14:48:40 -0400

    Completed exercice.py

--- exercice.py
+++ exercice.py
@@ -4,44 +4,74 @@
 
 def order(values: list = None) -> list:
     if values is None:
-        # TODO: demander les valeurs ici
-        pass
+        values = [input("Entrez un entier, float ou str: ") for _ in range(10)]
 
-    return []
+    return sorted(values)
+
+
+def count_letter_occurences(word: str) -> dict:
+    occurences = {}
+    for c in word:
+        if c in occurences:
+            occurences[c] += 1
+        else:
+            occurences[c] = 1
+
+    return occurences
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: demander les mots ici
-        pass
+        words = [input("Entrez un mot: ") for _ in range(2)]
 
-    return False
+    assert len(words) == 2, "There should be 2 words in the list"
+    return count_letter_occurences(words[0]) == count_letter_occurences(words[1])
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    return len(items) == len(set(items))
 
 
 def best_grades(student_grades: dict) -> dict:
-    # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    assert student_grades != {}, "No students in the dict"
+
+    best_student = None
+    best_average = 0.0
+    for k, v in student_grades.items():
+        average = sum(v) / len(v)
+        if average > best_average:
+            best_average = average
+            best_student = k
+
+    return {best_student: best_average}
 
 
 def frequence(sentence: str) -> dict:
-    # TODO: Afficher les lettres les plus fréquentes
-    #       Retourner le tableau de lettres
-
-    return {}
+    return count_letter_occurences(sentence)
 
 
-def get_recipes():
-    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+recipes = {}
+
+
+def get_recipes() -> None:
+    recipe_name = input("Le nom de la recette: ")
+    ingredients = input(
+        "Les ingrédients de la recette, séparé par des espaces: "
+    ).split()
+
+    global recipes
+    recipes[recipe_name] = ingredients
 
 
 def print_recipe(ingredients) -> None:
-    # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    recipe_name = input("Le nom de la recette: ")
+
+    global recipes
+    ingredients = recipes.get(recipe_name, None)
+    if ingredients is None:
+        print("There is no recipe")
+    else:
+        print(" ".join(ingredients))
 
 
 def main() -> None:
@@ -56,9 +86,13 @@ def main() -> None:
 
     grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
     best_student = best_grades(grades)
-    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+    print(
+        f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}"
+    )
 
-    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    sentence = (
+        "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    )
     frequence(sentence)
 
     print("On enregistre les recettes...")
@@ -68,5 +102,5 @@ def main() -> None:
     print_recipe(recipes)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
